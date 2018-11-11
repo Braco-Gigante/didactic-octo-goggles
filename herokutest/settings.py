a7d4717dd95341a52ff9db5b44a476af1c4bb4e6
fvendrameto <fernandovendrameto@gmail.com>
2018-11-10 22:39:20 -0200

    simpler

--- herokutest/settings.py
+++ herokutest/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'z!v4@nbpe5t-v&nq)@k_%rq+i7+fn(**&0ay=!eo-xk=ps%%-t'
+SECRET_KEY = 'z!v4@nbpe5t-v&nq)@k_%rq+i7+fn(**&0ay=!eo-xk=ps%%-t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
